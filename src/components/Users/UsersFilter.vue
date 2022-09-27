7c6f48ac274e6b5227ed931774728526d4abe279
suppolyar <vkhardy@inbox.ru>
2022-09-27 19:50:22 +0600

    Split the block into components

---
+++ src/components/Users/UsersFilter.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-col
+      cols="12"
+      md="4"
+  >
+    <v-select
+        v-model="variant"
+        :items="items"
+        clearable
+        label="Filter by country"
+    ></v-select>
+
+    <v-select
+        v-model="variant"
+        :items="items"
+        clearable
+        label="Filter by score"
+    ></v-select>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "UsersFilter",
+  data: () => ({
+    items: [
+      'russia',
+      'usa',
+      '> 20',
+      '< 10',
+    ],
+    variant: 'default',
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
