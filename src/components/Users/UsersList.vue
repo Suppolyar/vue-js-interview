c687596a240ffb13f34ec87ef57827a6d39e3e19
suppolyar <vkhardy@inbox.ru>
2022-09-28 16:45:32 +0600

    Added a extra info and transition

--- src/components/Users/UsersList.vue
+++ src/components/Users/UsersList.vue
@@ -10,8 +10,14 @@
 
         <template v-for="(item, index) in this.$props.list">
           <div :key="index">
-            <v-list-item>
-              <v-list-item-avatar>
+            <v-list-item >
+
+              <v-list-item-avatar
+                  class="v-btn avatar-btn"
+                  tabindex="1"
+                  @keyup.enter="openExtraInfo(index)"
+                  @click="openExtraInfo(index)"
+              >
                 <v-img :src="item.avatar" />
               </v-list-item-avatar>
 
@@ -30,6 +36,13 @@
                   <span class="blue--text">{{ item.country }}</span>
                 </v-list-item-content>
 
+                <transition name="fade">
+                  <v-list-item-content v-if="isOpenedIdx.indexOf(index) !== -1">
+                    <div class="font-weight-bold">Address:</div>
+                    <div>{{ item.address.city }}</div>
+                    <div>{{ item.address.street }}</div>
+                  </v-list-item-content>
+                </transition>
               </v-list-item-content>
 
             </v-list-item>
@@ -45,10 +58,36 @@
 
 export default {
   name: "UsersList",
-  props: ['list']
+  props: ['list'],
+  data() {
+    return {
+      isOpen: false,
+      isOpenedIdx: []
+    }
+  },
+  methods: {
+    openExtraInfo(idx) {
+      let index = this.isOpenedIdx.indexOf(idx)
+      if (index !== -1) {
+        this.isOpenedIdx.splice(index, 1)
+      } else {
+        this.isOpenedIdx.push(idx)
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.avatar-btn {
+  cursor: pointer;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 </style>
